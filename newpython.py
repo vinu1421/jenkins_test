165424f4d0c8481bdfb265e23a1e3288a5d7105f
vinayak <vinayak.prasannakumar@mindcurv.com>
2020-04-29 12:47:39 +0530

    another test

--- newpython.py
+++ newpython.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 import os
-import requests
-vinu = os.getenv("ServiceName")
+vinu = os.getenv("ServiceName").split(,)
 print(vinu)
 
-url_data = "http://omd.carrefour.es/c4omd/thruk/cgi-bin/status.cgi?view_mode=json&s0_op=%3D&s0_type=event+handler&s0_value=sf_restart&columns=host_name,description,event_handler,host_groups"
-response = requests.get(url_data)
-
-print(response)
