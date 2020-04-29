1be1784eaad406b91216f3c7871cd0157a26faeb
vinayak <vinayak.prasannakumar@mindcurv.com>
2020-04-29 17:51:46 +0530

    added a python script

--- newpython.py
+++ newpython.py
@@ -1,5 +1,42 @@
 #!/usr/bin/env python3
 import os
-vinu = os.getenv("ServiceName").split()
-print(vinu)
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+
+environment = os.getenv("C4Environment").split()
+EventHandler = os.getenv("EventHandler").split()
+ServiceName = os.getenv("ServiceName").split()
+omduser = 'omdguest'
+omdpass = 'omdguest'
+
+for eventhandlers in EventHandler:
+    
+    url_data = "http://omd.carrefour.es/c4omd/thruk/cgi-bin/status.cgi?view_mode=json&s0_op=%3D&s0_type=event+handler&s0_value=" + eventhandlers + "&columns=host_name,description,event_handler,host_groups"
+    response = requests.get(url_data, verify=False, auth=HTTPBasicAuth(omduser, omdpass))
+    json_data = json.loads(response.content)
+    
+    for data in json_data:
+        
+        hostgroup = data['host_groups'][0]
+        description = data['description']
+        hostname = data['host_name']
+        
+        if hostgroup in environment and description in ServiceName:
+            
+            api_url= "https://omdguest:omdguest@omd.carrefour.es/c4omd/thruk/r/v1/services/" + hostname + "/" + description + "/cmd/enable_svc_event_handler"
+            response1 = requests.post(api_url, verify=False)
+            print(hostname)
+            break
+  
+            break 
+                 
+                
+        
+        
+    
+    
+    
+
+
 
